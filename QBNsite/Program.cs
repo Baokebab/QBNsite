bdb1fa4c751a6c4c92890a6e587ab9ba45563447
Bao Nguyen <qbnguyen@outlook.fr>
2025-07-07 23:43:01 +0200

    Website rename

--- QBNsite/Program.cs
+++ QBNsite/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress + "QBNsite/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress + "QuocBaoNguyen/") });
 
 await builder.Build().RunAsync();
